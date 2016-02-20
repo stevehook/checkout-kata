aa8c904a6caf270e5e4fcf4c1a09bbd2b4e0f150
Steve Hook <steve.hook@gmail.com>
2016-02-20 17:57:22 +0000

    Implement TotalSpendPromotion

--- lib/checkout/total_spend_promotion.rb
+++ lib/checkout/total_spend_promotion.rb
@@ -1,1 +1,11 @@
-TotalSpendPromotion = Struct.new(:total_spend, :percentage_discount)
+class TotalSpendPromotion < Struct.new(:total_spend, :percentage_discount)
+  def apply_to_total(total)
+    total > total_spend ? discount_total(total) : total
+  end
+
+  private
+
+  def discount_total(total)
+    (total * (100 - percentage_discount)) / 100
+  end
+end
