f6ea48492e4c12fbf6c897b68abaeb7de4027205
Steve Hook <steve.hook@gmail.com>
2016-02-20 17:39:25 +0000

    Set up failing spec for total spend discount

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -9,8 +9,31 @@ RSpec.describe Checkout do
     expect(subject.total).to eql 0
   end
 
-  it 'can scan items' do
-    subject.scan('001')
-    expect(subject.total).to eql BigDecimal('9.25')
+  context 'without rules' do
+    it 'can scan items' do
+      subject.scan('001')
+      expect(subject.total).to eql BigDecimal('9.25')
+    end
+
+    it 'can scan multiple items' do
+      subject.scan('001')
+      subject.scan('002')
+      subject.scan('003')
+      expect(subject.total).to eql BigDecimal('74.20')
+    end
+  end
+
+  context 'with rules' do
+    it 'can scan items' do
+      subject.scan('001')
+      expect(subject.total).to eql BigDecimal('9.25')
+    end
+
+    it 'can scan multiple items and applies discount for total spend' do
+      subject.scan('001')
+      subject.scan('002')
+      subject.scan('003')
+      expect(subject.total).to eql BigDecimal('66.78')
+    end
   end
 end
